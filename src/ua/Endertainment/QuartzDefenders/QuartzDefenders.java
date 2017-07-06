c494891e53ed5f82cfa407a253dd85199e452c6d
Endertainment <volodyasolodkii2000@gmail.com>
2017-07-06 17:50:52 +0300

    11111111

--- src/ua/Endertainment/QuartzDefenders/QuartzDefenders.java
+++ src/ua/Endertainment/QuartzDefenders/QuartzDefenders.java
@@ -78,6 +78,10 @@ public class QuartzDefenders extends JavaPlugin {
         }
 
         /*
+         * sghsdgfsdgdfgdfgdfgdfgd
+         */
+        
+        /*
 		 * Register Events&Commands
          */
         registerEvents();
