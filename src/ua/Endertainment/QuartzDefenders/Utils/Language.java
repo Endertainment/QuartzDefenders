524733329b7d4473c881e2252d54e72a651dde48
Cool_boy <Coolboy.mc1@gmail.com>
2017-07-09 16:12:45 +0300

    Default language reading from .jar

--- src/ua/Endertainment/QuartzDefenders/Utils/Language.java
+++ src/ua/Endertainment/QuartzDefenders/Utils/Language.java
@@ -1,42 +1,60 @@
 package ua.Endertainment.QuartzDefenders.Utils;
 
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.nio.file.NoSuchFileException;
 import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.bukkit.Bukkit;
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.configuration.file.YamlConfiguration;
 import ua.Endertainment.QuartzDefenders.QuartzDefenders;
 
-public class Language {
+public abstract class Language {
 
-	public static void reload() {
-		QuartzDefenders.getInstance().getConfigs().reloadLang();
-	}
-	
-	public static String getRawString(String path) {
-		String s = QuartzDefenders.getInstance().getConfigs().getLang().getString(path);
-		if(s == null)  {
-                    Bukkit.getLogger().log(Level.WARNING, "Could not find string {0}", path);
-                    return " ";
-                }		
-		return s; 
-	}
-        
-        public static String getString(String path) {
-            return new ColorFormat(getRawString(path)).format();
+    public static void reload() {
+        QuartzDefenders.getInstance().getConfigs().reloadLang();
+    }
+
+    public static YamlConfiguration getDefaultLanguage() {
+        Reader reader = new InputStreamReader(QuartzDefenders.getInstance().getResource("messages_en.yml"));
+        return YamlConfiguration.loadConfiguration(reader);
+    }
+
+    public static String getRawString(String path) {
+        String s = QuartzDefenders.getInstance().getConfigs().getLang().getString(path);
+        if (s == null) {
+            s = getDefaultLanguage().getString(path);
+            if (s == null) {
+                Bukkit.getLogger().log(Level.WARNING, "Could not find string {0}", path);
+            }
+            return " ";
         }
-        
-        public static String getString(String path, Replacer... replacer) {
-            return new ColorFormat(getRawString(path, replacer)).format();
+        return s;
+    }
+
+    public static String getString(String path) {
+        return new ColorFormat(getRawString(path)).format();
+    }
+
+    public static String getString(String path, Replacer... replacer) {
+        return new ColorFormat(getRawString(path, replacer)).format();
+    }
+
+    public static String getRawString(String path, Replacer... replacer) {
+        String s = QuartzDefenders.getInstance().getConfigs().getLang().getString(path);
+        if (s == null) {
+            s = getDefaultLanguage().getString(path);
+            if (s == null) {
+                Bukkit.getLogger().log(Level.WARNING, "Could not find string {0}", path);
+            }
+            return " ";
         }
-        
-	public static String getRawString(String path, Replacer... replacer) {
-		String s = QuartzDefenders.getInstance().getConfigs().getLang().getString(path);
-		if(s == null)  {
-                    Bukkit.getLogger().log(Level.WARNING, "Could not find string {0}", path);
-                    return " ";
-                }
-		for(Replacer repl : replacer) {
-			s = new Replacer(s, repl.what(), repl.to()).getReplaced();
-		}		
-		return s;
-	}
-	
+        for (Replacer repl : replacer) {
+            s = new Replacer(s, repl.what(), repl.to()).getReplaced();
+        }
+        return s;
+    }
+
 }
