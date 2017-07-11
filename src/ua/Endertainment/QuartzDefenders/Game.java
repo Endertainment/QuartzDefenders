c58ac799b1a76befd7e9989d6b8acb8f0e2e3820
Endertainment <volodyasolodkii2000@gmail.com>
2017-07-11 18:41:08 +0300

    sdsdsdsdsdsdssds

--- src/ua/Endertainment/QuartzDefenders/Game.java
+++ src/ua/Endertainment/QuartzDefenders/Game.java
@@ -157,8 +157,7 @@ public class Game {
                 config.getDouble("Games." + this.id + ".map_spawn.y"),
                 config.getDouble("Games." + this.id + ".map_spawn.z") + 0.5);
         this.customShop = config.getBoolean("Games." + this.id + ".custom_shop", false);
-        this.buildCuboid = new BCub(new Location(map, mapSpawn.clone().getBlockX() + buildRadius, 0, mapSpawn.clone().getBlockZ() + buildRadius),
-                new Location(map, mapSpawn.clone().getBlockX() - buildRadius, buildHeight, mapSpawn.clone().getBlockZ() - buildRadius));
+        this.buildCuboid = new BCub(mapSpawn.clone().add(buildRadius, 0, buildRadius), mapSpawn.clone().subtract(buildRadius, buildHeight, buildRadius));
 
         int i = 0;
         for (String team : (List<String>) config.getList("Games." + this.id + ".teams")) {
