dfd90e9776a3767b93590661c7a6a4e71a2669aa
Endertainment <Vova@VovaPC>
2017-12-16 20:57:39 +0200

    kititem+

--- src/ua/endertainment/quartzdefenders/kits/KitItem.java
+++ src/ua/endertainment/quartzdefenders/kits/KitItem.java
@@ -1,5 +1,9 @@
 package ua.endertainment.quartzdefenders.kits;
 
+import java.util.Map;
+
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.inventory.ItemStack;
 
 public interface KitItem {
@@ -7,6 +11,14 @@ public interface KitItem {
 	public abstract String getKitID();
 	
 	public abstract String getItemID();
+	
+	public abstract Material getMaterial();
+	
+	public abstract int getAmount();
+	
+	public abstract short getDurability();
+	
+	public abstract Map<Enchantment, Integer> getEnchantments();
 	
 	public abstract ItemStack getItem();
 		
