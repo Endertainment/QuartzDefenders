e41f867015c8d2c2313aece92358004d6e46d91f
Endertainment <Vova@VovaPC>
2017-12-22 17:07:04 +0200

    color console messages

--- src/ua/endertainment/quartzdefenders/utils/LoggerUtil.java
+++ src/ua/endertainment/quartzdefenders/utils/LoggerUtil.java
@@ -1,24 +1,23 @@
 package ua.endertainment.quartzdefenders.utils;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import org.bukkit.Bukkit;
+import org.bukkit.command.ConsoleCommandSender;
 
 public abstract class LoggerUtil {
 
     private static String prefix = new ColorFormat("&b&lQuartz &3\u00BB &8: &f").format();
-    private final static Logger LOGGER = Bukkit.getLogger();
+    private final static ConsoleCommandSender LOGGER = Bukkit.getConsoleSender();
 
     public static String getPrefix() {
         return prefix;
     }
 
     public static void logError(String message) {
-        LOGGER.log(Level.SEVERE, new ColorFormat(prefix + "&4 " + Language.getString("logger.error") + " &8: &4" + message).format());
+        LOGGER.sendMessage(new ColorFormat(prefix + "&4 " + Language.getString("logger.error") + " &8: &4" + message).format());
     }
 
     public static void logInfo(String message) {
-        LOGGER.log(Level.INFO, new ColorFormat(prefix + "&7 " + Language.getString("logger.info") + " &8: &7" + message).format());
+        LOGGER.sendMessage(new ColorFormat(prefix + "&7 " + Language.getString("logger.info") + " &8: &7" + message).format());
     }
 
     public static String gameMessage(String type, String message) {
